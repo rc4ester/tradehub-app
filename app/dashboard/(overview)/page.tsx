adf45cc01588109c414d36302afea2b3a330961b
Richard Forrester <rc4ester@gmail.com>
2024-07-02 21:01:25 -0500

    Completed Dashboard

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,35 @@
+import CardWrapper from '@/app/ui/dashboard/cards';
+import IncomeChart from '@/app/ui/dashboard/income-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { poppins } from '@/app/ui/fonts';
+import { IncomeChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+// import { fetchCardData } from '@/app/lib/data';
+
+export default async function Page() {
+    // const {numberOfSellers, numberOfInvoices, totalFulfilledInvoices, totalAwaitingInvoices} = await fetchCardData();
+
+    return (
+        <main className="rounded-xl bg-neutral-900 p-6">
+        <h1
+            className={`${poppins.className} mb-4 text-center text-xl text-white md:text-3xl`}
+        >
+            Dashboard
+        </h1>
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+            <Suspense fallback={<CardsSkeleton/>}>
+                <CardWrapper />
+            </Suspense>
+        </div>
+        <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+            <Suspense fallback={<IncomeChartSkeleton/>}>
+                <IncomeChart />
+            </Suspense>
+            <Suspense fallback={<LatestInvoicesSkeleton/>}>
+                <LatestInvoices />
+            </Suspense>
+        </div>
+        </main>
+    );
+}
+  
